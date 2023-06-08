64a92672c74de559499db669641f0ac3e86397fb
Craig Pearson <thepearson@gmail.com>
2023-06-09 05:57:41 +1200

    Tidied up some config

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
+import PocketBase from 'pocketbase'
 
-export default function Home() {
+const pocketbase = new PocketBase('http://database:8080');
+
+export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
